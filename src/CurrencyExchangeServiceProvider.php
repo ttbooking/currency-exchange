810110d34520e588ded6797afa2ba3976d929679
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2022-10-25 12:52:35 +0200

    Some fixes

--- src/CurrencyExchangeServiceProvider.php
+++ src/CurrencyExchangeServiceProvider.php
@@ -5,18 +5,7 @@ declare(strict_types=1);
 namespace TTBooking\CurrencyExchange;
 
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use TTBooking\CurrencyExchange\Http\Controllers\ExchangeRateController;
-
-//use TTBooking\CurrencyExchange\Contracts\ExchangeRateProvider;
-//use TTBooking\CurrencyExchange\Providers\Chain;
-//use TTBooking\CurrencyExchange\Providers\ExchangeRateCachingDecorator;
-//use TTBooking\CurrencyExchange\Providers\ExchangeRatePDOStore;
-//use TTBooking\CurrencyExchange\Providers\Identity;
-//use TTBooking\CurrencyExchange\Providers\NationalBankOfRepublicBelarus;
-//use TTBooking\CurrencyExchange\Providers\ReversibleExchangeRateProvider;
-//use TTBooking\CurrencyExchange\Providers\RussianCentralBank;
 
 class CurrencyExchangeServiceProvider extends ServiceProvider implements DeferrableProvider
 {
@@ -27,26 +16,10 @@ class CurrencyExchangeServiceProvider extends ServiceProvider implements Deferra
      */
     public function boot(): void
     {
-        $this->registerRoutes();
-
         if ($this->app->runningInConsole()) {
             $this->offerPublishing();
             $this->registerMigrations();
         }
-    }
-
-    protected function registerRoutes(): void
-    {
-        Route::group([
-            'domain' => $this->app['config']['currency-exchange.domain'] ?? null,
-            'prefix' => $this->app['config']['currency-exchange.path'] ?? null,
-            'name' => 'currency-exchange.',
-            'namespace' => 'TTBooking\\CurrencyExchange\\Http\\Controllers',
-            'middleware' => $this->app['config']['currency-exchange.middleware'] ?? 'api',
-        ], function () {
-            Route::get('/rate/{base}/{quote}', [ExchangeRateController::class, 'getExchangeRate'])
-                ->where(['base' => '[A-Z]{3}', 'quote' => '[A-Z]{3}']);
-        });
     }
 
     protected function offerPublishing(): void
@@ -81,25 +54,6 @@ class CurrencyExchangeServiceProvider extends ServiceProvider implements Deferra
         $this->app->singleton('currency-exchange.provider', function ($app) {
             return $app['currency-exchange']->driver();
         });
-
-        /*$this->app->singleton(ExchangeRateProvider::class, function ($app) {
-            return new Identity(
-                new Chain([
-                    new ReversibleExchangeRateProvider(
-                        new ExchangeRateCachingDecorator(
-                            new NationalBankOfRepublicBelarus,
-                            new ExchangeRatePDOStore($app['db']->getPdo(), 'exchange_rates')
-                        )
-                    ),
-                    new ReversibleExchangeRateProvider(
-                        new ExchangeRateCachingDecorator(
-                            new RussianCentralBank,
-                            new ExchangeRatePDOStore($app['db']->getPdo(), 'exchange_rates')
-                        )
-                    ),
-                ])
-            );
-        });*/
     }
 
     /**
